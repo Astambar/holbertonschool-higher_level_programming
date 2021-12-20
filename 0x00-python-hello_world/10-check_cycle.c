55f1a81d12a8974f1f36d196ac8198a0b03eeb54
Astambar <leuchartjason@gmail.com>
2021-12-20 16:03:31 +0000

    checker

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,7 +13,7 @@ int check_cycle(listint_t *list)
 
 	simple_node = list, double_node = list;
 
-	for (; simple_node && double_node && double_node->next simple_node = simple_node->next)
+	for (; simple_node && double_node && double_node->next; simple_node = simple_node->next)
 	{
 		if (simple_node == double_node)
 			return (1);
