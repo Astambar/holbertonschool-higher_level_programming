d7e41139e55b73631e523a150d1a25dc4c37bdf5
Astambar <leuchartjason@gmail.com>
2021-12-20 15:30:02 +0000

    initialisation project

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - detecte si une list est un cycle
+ * @list: list chaîner tester
+ * Return: 1 si c'est un cycle et 0 si ce n'est pas le cas
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *simple_node, *double_node;
+
+	simple_node = list, double_node = list;
+
+	for (; simple_node && double_node; simple_node = simple_node->next)
+	{
+		double_node = double_node->next->next
+
+		if (simple_node == double_node)
+			return (1);
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print('{:.2f} Battery street'.format(3.14159))
