cdc820b8789d41b8df5bc18cb43910838098f776
Astambar <leuchartjason@gmail.com>
2021-12-21 15:45:31 +0000

    second tenta

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     modulo = number % 10
 chardefault = 'Last digit of {:d} is {:d} '.format(number, modulo)
 if modulo > 5:
-    print('{:s} and is greater than 5'.format(chardefault))
+    print('{:s} and is greater than 5'.format(chardefault), end="")
 elif modulo == 0:
-    print('{:s} and is 0'.format(chardefault))
+    print('{:s} and is 0'.format(chardefault), end="")
 else:
-    print('{:s} and is less than 6 and not 0'.format(chardefault))
+    print('{:s} and is less than 6 and not 0'.format(chardefault), end="")
