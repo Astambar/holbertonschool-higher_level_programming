b0017358ca4e60e5cb03957900eea0dd674ee4bc
Astambar <leuchartjason@gmail.com>
2021-12-21 17:18:11 +0000

    first tenta

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-	newstr = ""
-	for i in str[:]:
-		c = ord(i)
-		if c >= ord("a") and c <= ord("z") + 1:
-			w = chr(c - 32)
-		else:
-			w = c
-		newstr += '{:s}'.format(w)
-	print("{:s}".format(newstr), end="\n")
+    for c in str[:]:
+        val = ord(c)
+        if val >= 97 and val <= 122:
+            w = chr(val - 32)
+        else:
+            w = c
+        print('{:s}'.format(w), end="")
+    print()
