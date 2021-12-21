2c31dd17152efe382a75e005fa19a70380442fcd
Astambar <leuchartjason@gmail.com>
2021-12-21 15:59:06 +0000

    mini correctif checker

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z") + 1):
-	if(ord("e") != i and ord("q") != i):
-		print("{:s}".format(chr(i)), end="")
-
-	
+    if(ord("e") != i and ord("q") != i):
+        print("{:s}".format(chr(i)), end="")
