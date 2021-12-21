a4a27b973dd88aa079e7835ec9dd7f5ad618e352
Astambar <leuchartjason@gmail.com>
2021-12-21 18:52:16 +0000

    first tenta

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    string = ""
+    for i in range(1, 101):
+        if i % 3 == 0:
+            string += 'Fizz '
+        if i % 5 == 0:
+            string += 'Buzz '
+        if i % 3 != 0 and i % 5 != 0:
+            string += '{:d} '.format(i)
+    print(string)
