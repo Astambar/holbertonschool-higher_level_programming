0f3cd5c6ab8a239b5bfa0e85c65b9b7500f00f63
Astambar <leuchartjason@gmail.com>
2021-12-21 16:18:51 +0000

    first tenta

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z")+1):
-    if(ord("e") != i and ord("e") != i):
-        print("{:s}".format(i), end="")
+char = ""
+for i in range(0, 99):
+    char += '{:d}{:d}'.format(i // 10, i % 10) + ', '
+char += '{:d}{:d}'.format((i + 1) // 10, (i + 1) % 10)
+
+print(char)
