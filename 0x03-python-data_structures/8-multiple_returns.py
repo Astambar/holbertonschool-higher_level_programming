8f4d721a6fb41de9fb27710b57b14ec9d42bb00f
Astambar <leuchartjason@gmail.com>
2022-01-03 22:23:15 +0000

    second tenta

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     lensentence = len(sentence)
-    return lensentence, None if lensentence == 0 else lensentence, sentence[0]
+    return (lensentence, sentence[0] if lensentence > 0 else None)
