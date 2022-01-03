d05335af0f0a3c18b6298829d498258c8eb7fa12
Astambar <leuchartjason@gmail.com>
2022-01-03 14:31:43 +0000

    Checker is my friend2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
 	newstring =""
 	if my_string:
 		for i in my_string:
-			if my_string[i] != "C" and my_string[i] != "c":
+			if my_string[i] not in 'Cc':
 				newstring += my_string[i]
 		return newstring
