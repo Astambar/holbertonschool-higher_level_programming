dda0bdbb01e3e33192b3fa80c222af1d4cfc988f
Astambar <leuchartjason@gmail.com>
2022-01-05 21:54:28 +0000

    first tenta

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     num_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_val = 0
-    for word in roman_string:
+    for word in list(roman_string):
         for key, value in num_roman.items():
             if key == word:
                 total += value
