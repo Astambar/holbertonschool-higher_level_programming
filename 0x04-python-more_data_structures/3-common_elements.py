c2a200f9f210d3b4f431284d9f73234bb37eed4d
Astambar <leuchartjason@gmail.com>
2022-01-05 14:47:31 +0000

    init project

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+	if set_1:
+		if set_2:
+			for i in set_1:
+				for j in set_2:
+					if i == j:
+						return i
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
