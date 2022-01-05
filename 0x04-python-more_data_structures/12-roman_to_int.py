40fa93d5bad484066deca5cc4c55826733b96957
Astambar <leuchartjason@gmail.com>
2022-01-05 22:08:23 +0000

    second tenta

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,21 @@
 def roman_to_int(roma_string):
     if roma_string is None or isinstance(roma_string, str) is False:
         return 0
-    letters = {'I': 1, 'V': 5,  'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    reverse_num = roma_string[::-1]
+    roman_number = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000
+                }
     maxchar = 'I'
-    total = 0
-    for i in reverse_num:
-        if letters[i] >= letters[maxchar]:
+    result = 0
+    for i in roma_string[::-1]:
+        if roman_number[i] >= roman_number[maxchar]:
             maxchar = i
-            total += letters[i]
+            result += roman_number[i]
         else:
-            total -= letters[i]
-    return total
+            result -= roman_number[i]
+    return result
