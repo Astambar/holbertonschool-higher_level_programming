775c5104199102c20a60d86594774dcec29f3e47
Astambar <leuchartjason@gmail.com>
2022-01-05 21:32:03 +0000

    first tenta

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
     keys = list(a_dictionary.keys())
-    maxnumber = keys[0]
+    maxnum = keys[0]
     for num in keys:
         maxnum = num if a_dictionary[num] > a_dictionary[maxnum] else maxnum
     return maxnumber
