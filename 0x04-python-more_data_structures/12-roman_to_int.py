ee824c65fb2e82c60a2f3254ef4d0cfd43d49c13
Astambar <leuchartjason@gmail.com>
2022-01-05 21:56:11 +0000

    first tenta

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,14 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return None
-    num_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_val = 0
-    for word in enumerate(roman_string):
-        for key, value in num_roman.items():
-            if key == word:
-                total += value
-                if prev_val < value:
+    for i in range(len(roman_string)):
+        for key, val in num.items():
+            if key == roman_string[i]:
+                total += val
+                if prev_val < val:
                     total -= (prev_val * 2)
-                    prev_val = value
+                    prev_val = val
     return total
