481cedae46d28267e670f4bfdce5741906b655c6
Astambar <leuchartjason@gmail.com>
2022-01-05 15:02:03 +0000

    first tenta

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-	if set_1:
-		if set_2:
-			tab = []
-			for i in set_1:
-				for j in set_2:
-					if i == j:
-						tab.append(i)
-			return tab
+	return set_1 & set_2
