d208f180020f42c937fd434a10e825c0c0c654e2
Astambar <leuchartjason@gmail.com>
2022-01-17 15:14:48 +0000

    third tenta

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     increment = 0
-    string = ""
     for i in range(x):
         try:
-            string += "{:d}".format(my_list[i])
+            print("{:d}".format(my_list[i]), end="")
             increment += 1
         except (TypeError, ValueError):
             continue
-    print(string)
+    print()
     return (increment)
+my_list = [1, 2, 3, 4, 5, 8]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
