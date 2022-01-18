87d9e3fc6ef9edf831741acebda55b4a2f7ce209
Astambar <leuchartjason@gmail.com>
2022-01-18 21:31:34 +0000

    third tenta

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,14 +58,16 @@ class Square:
         """Prints to stdout the square with the character #,
         at the position given by the position attribute.
         """
+        string = ""
         if self.__size == 0:
-            print()
+            string += "\n"
         else:
             for y in range(0, self.__position[1]):
-                print()
+                string += "\n"
             for i in range(0, self.__size):
                 for x in range(0, self.__position[0]):
-                    print(" ", end="")
+                    string += " "
                 for j in range(0, self.__size):
-                    print("#", end="")
-                print()
+                    string += "#"
+                string += "\n"
+            print(string)
