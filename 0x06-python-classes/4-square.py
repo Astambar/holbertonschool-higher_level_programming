05bb7a3dfff19e52789cc3383673adca81dcb4b7
Astambar <leuchartjason@gmail.com>
2022-01-18 20:02:19 +0000

    second tenta

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,26 +3,25 @@
 
 
 class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Instantiation with optional size.
-    Public instance method: def area(self).
+    """Représente un carré.
+    Attribut d'instance privée : size :
+        - Taille de la propriété (self)
+        - Pour modifier la taille def size(self, value)
+    Instanciation avec taille par default.
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
+        """Initialise la classe Square"""
         self.__size = size
 
     @property
     def size(self):
-        """Retrieves the size."""
+        """Récupére la taille du carrée"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size to a value."""
+        """Définit la nouvelle de taille du carrée"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
