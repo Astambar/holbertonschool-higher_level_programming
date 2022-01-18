599a6245181a08909ee907c9c32c0ec981c36a0d
Astambar <leuchartjason@gmail.com>
2022-01-18 21:05:33 +0000

    second tenta

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,31 +3,31 @@
 
 
 class Square:
-    """Représente un carré.
-    Attribut d'instance privée : size :
-        - Définition de la propriété size(self)
-        - Pour modifier la taille def size(self, value)
-    Attribut d'instance privée : position :
-        -  Définition de la propriété position(self)
-        - propriété setter def position(self, value)
-    Instanciation avec taille par default.
-    Méthode d'instance publique : def area(self).
-    Méthode d'instance publique : def my_print(self).
+    """Represents a square.
+    Private instance attribute: size:
+        - property def size(self)
+        - property setter def size(self, value)
+    Private instance attribute: position:
+        - property def position(self)
+        - property setter def position(self, value)
+    Instantiation with optional size and optional position.
+    Public instance method: def area(self).
+    Public instance method: def my_print(self).
     """
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialise la classe Square"""
+        """Initializes the data."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Récupére la taille du carrée"""
+        """Retrieves the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Définit la nouvelle de taille du carrée"""
+        """Sets the size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -51,20 +51,21 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Retourne la surface du carrée"""
+        """Returns the current square area."""
         return self.__size ** 2
 
     def my_print(self):
-        """Imprime sur la sortie standard
-        le carré avec le caractère #.
+        """Prints to stdout the square with the character #,
+        at the position given by the position attribute.
         """
-        string = ""
         if self.__size == 0:
             print()
-        else:
-            for i in range(0, self.__size):
-                for j in range(0, self.__size):
-                    string += "#"
-                if i < self.__size:
-                    string += '\n'
-            print(string, end="")
+            return
+        for y in range(0, self.__position[1]):
+            print()
+        for i in range(0, self.__size):
+            for x in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
