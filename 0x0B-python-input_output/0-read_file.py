27a92f4e970c22299d5d105803b11aac5e77708d
Astambar <leuchartjason@gmail.com>
2022-02-01 22:11:21 +0000

    push

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,5 @@
 
 
 def read_file(filename=""):
-    """[summary]
-
-    Args:
-        filename (str, optional): [description]. Defaults to "".
-    """
     with open(filename, encoding='UTF-8') as file:
         print(file.read(), end="")
