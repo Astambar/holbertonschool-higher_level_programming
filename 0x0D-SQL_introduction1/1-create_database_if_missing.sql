dd171b025bef1916f62407cc05aab480810115d3
Astambar <leuchartjason@gmail.com>
2022-03-08 23:24:47 +0000

    first tenta mandatory

---
+++ 0x0D-SQL_introduction1/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- script qui  génére la base de donnée si elle  existe pas
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction1/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- script qui supprime la base de donnée si elle existe pas
+DROP DATABASE IF EXISTS hbtn_0c_0;
