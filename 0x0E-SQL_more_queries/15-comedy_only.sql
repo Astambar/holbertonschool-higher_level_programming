7d27fc22b5d0fea9dfb85310a9daf487ac611539
Astambar <leuchartjason@gmail.com>
2022-03-09 23:01:29 +0000

    first tenta

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,4 @@
--- Import the database dump from hbtn_0d_tvshows
--- script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- script créer la database puis génére la table
 SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
