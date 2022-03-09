6bed27f836d16849119a842fd230b6bd15c05e44
Astambar <leuchartjason@gmail.com>
2022-03-09 23:01:10 +0000

    first tenta

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,3 @@
 
--- Import the database dump from hbtn_0d_tvshows
--- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- script créer la database puis génére la table
 SELECT name FROM tv_genres WHERE id IN (SELECT genre_id FROM tv_show_genres WHERE show_id = (SELECT id FROM tv_shows WHERE title='Dexter')) ORDER BY name;
