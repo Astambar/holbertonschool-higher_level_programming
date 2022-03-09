5869e6eba39c30fb27c6bf5453507888122ba712
Astambar <leuchartjason@gmail.com>
2022-03-09 23:00:23 +0000

    first tenta

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,2 @@
--- Import the database dump of hbtn_0d_tvshows
--- script that lists all shows contained in the database hbtn_0d_tvshows
+-- script créer la database puis génére la table
 SELECT ts.title, tsg.genre_id FROM tv_shows ts LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id ORDER BY ts.title, tsg.genre_id;
