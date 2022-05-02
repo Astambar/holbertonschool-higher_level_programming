ec15493e5fd376ab55cb62ea043e7963ce8e2a32
Astambar <leuchartjason@gmail.com>
2022-05-02 20:59:38 +0000

    0x03-python-datastructure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     newstring = ""
     if my_string:
         for i in range(0, len(my_string), 1):
-            if my_string[i] in ["C","c"]:
+            if my_string[i] != "C" and my_string[i] != "c":
                 newstring += my_string[i]
         return newstring
     return my_string
