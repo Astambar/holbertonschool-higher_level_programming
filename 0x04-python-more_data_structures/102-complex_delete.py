f9eb95f05aad868ca78947fcdd7b0b27dc8cc389
Astambar <leuchartjason@gmail.com>
2022-05-04 21:22:06 +0000

    push project 0x04-python-more_data_structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return None
-    keys = list(a_dictionary.keys())
-    maxnum = keys[0]
-    for num in keys:
-        maxnum = num if a_dictionary[num] > a_dictionary[maxnum] else maxnum
-    return maxnum
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
