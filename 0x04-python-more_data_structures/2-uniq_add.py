b24a7e6583d9947bd9df5a3a7563054fac9d6583
Astambar <leuchartjason@gmail.com>
2022-05-04 21:00:36 +0000

    push project 0x04-python-more_data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""function that returns a set of common elements in two sets
+    Return:
+        the addition of all unique elemnts
+"""
+
 def uniq_add(my_list=[]):
 	if my_list:
-		return sum(i for i in list(my_list))
+		return sum(i for i in set(my_list))
