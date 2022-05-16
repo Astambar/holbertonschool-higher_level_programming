20ec7f570207b9efd0a61a99e15fe43b474bfe47
Astambar <leuchartjason@gmail.com>
2022-05-16 11:01:19 +0000

    project mandatory 0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
-import contextlib
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     increment = 0
     string = ""
-    with contextlib.suppress(IndexError):
+    try:
         for i in range(x):
             string += f"{my_list[i]}"
             increment += 1
-
+    except IndexError:
+        pass
     print(string)
     return increment
