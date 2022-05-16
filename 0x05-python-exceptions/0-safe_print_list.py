10e500f7c7dd950be815ee3a94dea9b120346741
Astambar <leuchartjason@gmail.com>
2022-05-16 11:02:37 +0000

    project mandatory 0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    increment = 0
     string = ""
     try:
         for i in range(x):
             string += f"{my_list[i]}"
+            increment += 1
     except IndexError:
         pass
     print(string)
-    return i
+    return increment
