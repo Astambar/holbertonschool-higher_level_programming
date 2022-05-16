40050dddcf4f3e3d81d20fdc8261ecead72a3446
Astambar <leuchartjason@gmail.com>
2022-05-16 12:41:52 +0000

    checker intterogation

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return result
     finally:
-        print("Inside result: {}".format(result))
+        print(f"Inside result: {result}")
     return result
