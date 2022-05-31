19eeed8a62f7777337f76c4a9f5e5bad3a1465a2
Astambar <leuchartjason@gmail.com>
2022-05-31 20:06:41 +0000

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,43 @@
 #!/usr/bin/python3
-"""[summary]
 """
-import contextlib
+Take stdin and check the input, make some operation on it
+"""
+import sys
 
-def print_stats(size, status_codes):
-    """
-    The print_stats function prints the size of the file and
-    a count of each status code returned by the web server.
-    Args:
-        size (int): The size of the file in bytes.
-        status_codes (dict): A dictionary containing
-        all HTTP response codes and their counts.
-     Returns: None
-    :param size: Print the size of the file
-    :param status_codes: Store the status codes of the http requests
-    :return: A dictionary with the file size and a dictionary of status codes
-    :doc-author: Trelent
-    """
-    print(f"File size: {size}")
-    for key in sorted(status_codes):
-        print(f"{key}: {status_codes[key]}")
+errorCode = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+numOfLine = 0
+sumSize = 0
 
+try:
+    for line in sys.stdin:
+        lineToken = line.split()
+        if len(lineToken) >= 2:
+            tmp = numOfLine
+            if lineToken[-2] in errorCode:
+                errorCode[lineToken[-2]] += 1
+            numOfLine += 1
+            sumSize += int(lineToken[-1])
+        if numOfLine % 10 == 0:
+            print("File size: {:d}".format(sumSize))
+            for key, value in sorted(errorCode.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(sumSize))
+    for key, value in sorted(errorCode.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-if __name__ == "__main__":
-    import sys
-
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            with contextlib.suppress(IndexError, ValueError):
-                size += int(line[-1])
-
-            with contextlib.suppress(IndexError):
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+except KeyboardInterrupt:
+    print("File size: {:d}".format(sumSize))
+    for key, value in sorted(errorCode.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
