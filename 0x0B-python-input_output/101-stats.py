3a86842422898efe6bfd6a4a332b06d25bc54c84
Astambar <leuchartjason@gmail.com>
2022-05-31 20:03:55 +0000

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,24 +4,17 @@
 
 
 def print_stats(size, status_codes):
-    """
-    The print_stats function prints the size of the file and
-    a count of each status code returned by the web server.
-    Args:
-        size (int): The size of the file in bytes.
-        status_codes (dict): A dictionary containing
-        all HTTP response codes and their counts.
-     Returns: None
-    :param size: Print the size of the file
-    :param status_codes: Store the status codes of the http requests
-    :return: A dictionary with the file size and a dictionary of status codes
-    :doc-author: Trelent
-    """
-    print(f"File size: {size}")
-    for key in sorted(status_codes):
-        print(f"{key}: {status_codes[key]}")
+    """[summary]
 
-import contextlib
+    Args:
+        size ([type]): [description]
+        status_codes ([type]): [description]
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
 if __name__ == "__main__":
     import sys
 
@@ -40,15 +33,19 @@ if __name__ == "__main__":
 
             line = line.split()
 
-            with contextlib.suppress(IndexError, ValueError):
+            try:
                 size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-            with contextlib.suppress(IndexError):
+            try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
                         status_codes[line[-2]] = 1
                     else:
                         status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
         print_stats(size, status_codes)
 
