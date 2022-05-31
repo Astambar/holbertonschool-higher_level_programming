9b89701d73e4582a36a95fc9b752fa30d32983cc
Astambar <leuchartjason@gmail.com>
2022-05-31 13:47:10 +0000

    mandatory task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,15 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ Using and save the save_to_json_file """
+    """
+    The save_to_json_file function writes an object to a text file,
+    using a JSON representation.
+
+    :param my_obj: Store the object that will be saved in the json file
+    :param filename: Specify the file name to which the data is saved
+    :return: None
+    :doc-author: Trelent
+    """
+
     with open(filename, mode="w", encoding="utf-8") as save_to_json_file:
         save_to_json_file.write(json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,13 @@ import json
 
 
 def load_from_json_file(filename):
-    """ json load """
+    """
+    The load_from_json_file function reads a json file and returns the object
+
+    :param filename: Specify the file to load from
+    :return: The content of the file in a string format
+    :doc-author: Trelent
+    """
+
     with open(filename, mode="r", encoding="utf-8") as load_from_json_file:
         return json.load(load_from_json_file)
