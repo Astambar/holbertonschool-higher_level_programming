7b24fcf79ca284074bc9fba14a7dadc57e345601
Astambar <leuchartjason@gmail.com>
2022-05-31 13:18:43 +0000

    mandatory task

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,15 @@
 
 
 def write_file(filename="", text=""):
-    """ write this """
+    """
+    The write_file function writes a string to a file.
+
+    :param filename=&quot;&quot;: Specify the name of the file
+    that will be written to
+    :param text=&quot;&quot;: Pass a string to the function
+    :return: None
+    :doc-author: Trelent
+    """
+
     with open(filename, mode="w", encoding="utf-8") as write_file:
         return write_file.write(text)
