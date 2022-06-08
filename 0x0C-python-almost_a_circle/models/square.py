22c5ceb8bd685133f011df2c5e970e65798d7529
Astambar <leuchartjason@gmail.com>
2022-06-08 14:00:06 +0000

    style reform

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -132,17 +132,3 @@ class Square(Rectangle):
         self.see_value("width", value)
         self.width = value
         self.height = value
-
-
-if __name__ == "__main__":
-
-    s1 = Square(5)
-    print(s1)
-    print(s1.size)
-    s1.size = 10
-    print(s1)
-
-    try:
-        s1.size = "9"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
