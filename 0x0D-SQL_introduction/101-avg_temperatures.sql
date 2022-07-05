c9ad537a521b16bd4f0fbd00b8431eb15707269a
Astambar <leuchartjason@gmail.com>
2022-07-05 19:25:32 +0000

    101-avg_temperatures.sql second

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
--- script qui affiche la température moyenne (Fahrenheit)
--- par ville classée par température (décroissant)
-SELECT `city`, AVG( `value` ) AS `tempAVG`
+-- script qui affiche la température moyenne (Fahrenheit) par ville classée par température (décroissant)
+SELECT `city`, AVG( `value` ) AS `avg_temp`
 FROM `temperatures`
 GROUP BY `city`
-ORDER BY `tempAVG` DESC;
+ORDER BY `avg_temp` DESC;
