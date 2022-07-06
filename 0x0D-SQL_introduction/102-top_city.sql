205fea0d4213647375c8d4cf89e6de3819f72c88
Astambar <leuchartjason@gmail.com>
2022-07-06 16:45:20 +0000

    primo

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,5 @@
--- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+-- script qui affiche le top 3 des températures
+-- des villes en juillet et août classés par température (descroissant)
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
