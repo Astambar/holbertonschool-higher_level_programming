fa27ba3b170b97272d6f4a9382e94240cbf6de02
Astambar <leuchartjason@gmail.com>
2022-07-06 18:42:00 +0000

    101-not_a_comedy.sql second

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,15 +1,15 @@
 -- script qui utilise la base de données hbtn_0d_tvshows pour lister
 -- tous les genres non liés au genre Comedy
-SELECT tv_genres.name
-FROM tv_genres
-WHERE tv_genres.name NOT IN
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN
 (
-	SELECT tv_genres.name
-	FROM tv_genres
+	SELECT tv_shows.title
+	FROM tv_shows
 	LEFT JOIN tv_show_genres
-	ON tv_genres.id=tv_show_genres.genre_id
-	LEFT JOIN tv_shows
-	ON tv_show_genres.show_id=tv_shows.id
+	ON tv_shows.id=tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_show_genres.genre_id=tv_genres.id
 	WHERE tv_genres.name='Comedy'
 )
-ORDER BY tv_genres.name;
+ORDER BY tv_shows.title;
