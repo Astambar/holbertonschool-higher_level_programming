fce9516cdc146e4566d755975a63c0f310086bbd
Astambar <leuchartjason@gmail.com>
2022-07-06 18:30:32 +0000

    100-not_my_genres.sql second

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,21 +1,15 @@
--- Lists all genres not linked to
--- the show
-
-SELECT DISTINCT tv_genres.name
-  FROM tv_shows
-       JOIN tv_show_genres
-       ON tv_shows.id = tv_show_genres.show_id
-
-       JOIN tv_genres
-       ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name NOT IN (
-    SELECT tv_genres.name
-    FROM tv_shows
-         JOIN tv_show_genres
-         ON tv_shows.id = tv_show_genres.show_id
-
-         JOIN tv_genres
-         ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_shows.title = 'Dexter'
+--  script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+--  script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexte
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN
+(
+	SELECT tv_genres.name
+	FROM tv_genres
+	LEFT JOIN tv_show_genres
+	ON tv_genres.id=tv_show_genres.genre_id
+	LEFT JOIN tv_shows
+	ON tv_show_genres.show_id=tv_shows.id
+	WHERE tv_shows.title='Dexter'
 )
-ORDER BY tv_genres.name ASC;
+ORDER BY tv_genres.name;
