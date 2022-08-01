a97ce4b6f6f13384816ff169c4579d62d3aa90f4
Astambar <leuchartjason@gmail.com>
2022-08-01 19:55:51 +0000

    mandatory task first

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const args = process.argv;
+if (args[2] != null) {
+  console.log(args[2]);
+} else {
+  console.log('No argument');
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const args = process.argv;
+const x = parseInt(args[2]);
+if (Number.isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+}
