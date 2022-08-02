112137691f91463064dc55b9d03abed5ecb934ca
Astambar <leuchartjason@gmail.com>
2022-08-02 21:17:31 +0000

    project 13

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,30 +4,30 @@ class Rectangle that defines a rectangle
 */
 class Rectangle {
   constructor (w, h) {
-	  if (parseInt(w) && w > 0 && parseInt(h) && h > 0) {
+    if (parseInt(w) && w > 0 && parseInt(h) && h > 0) {
       this.width = w;
       this.height = h;
-	  }
+    }
   }
 
   print () {
-	  for (let y = 0; y < this.height; y++) {
+    for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-		  process.stdout.write('X');
+        process.stdout.write('X');
       }
       process.stdout.write('\n');
-	  }
+    }
   }
 
   rotate () {
-	  const x = this.width;
-	  this.width = this.height;
-	  this.height = x;
+    const x = this.width;
+    this.width = this.height;
+    this.height = x;
   }
 
   double () {
-	  this.width *= 2;
-	  this.height *= 2;
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
