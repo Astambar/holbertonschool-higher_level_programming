6f44d316c6592172b1151e28ccf75b1d54339212
Astambar <leuchartjason@gmail.com>
2022-08-07 21:56:24 +0000

    correctif present

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
-"""
+"""script that adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -9,14 +7,8 @@ from sqlalchemy import insert
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-            ),
-        pool_pre_ping=True
-    )
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}', pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     session = Session(engine)
 
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
-"""
+
 import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
-from sqlalchemy import insert
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
