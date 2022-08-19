a18e45fe7f48aa6391be2bad45c55c29950dbb7c
Astambar <leuchartjason@gmail.com>
2022-08-19 20:53:24 +0000

    0x11 project correctif pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,10 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        letter = sys.argv[1]
-    else:
-        letter = ""
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
     data = {'q': letter}
 
     res = requests.post("http://0.0.0.0:5000/search_user", data=data)
