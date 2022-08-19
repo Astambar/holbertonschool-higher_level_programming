6e5383cc26a7c26c948ab7c3e7f8562ca431b367
Astambar <leuchartjason@gmail.com>
2022-08-19 21:00:16 +0000

    0x11 project correctif pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-'''Python script that sends a request to the URL'''
+'''Script Python qui envoie une requête à l'URL'''
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    res = requests.get(f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits')
-
+    res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[2],
+        sys.argv[1])
+    )
     if res.status_code >= 400:
         print('None')
     else:
