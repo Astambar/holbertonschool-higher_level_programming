157422011f253ed295c497897b85a0942b238c89
Astambar <leuchartjason@gmail.com>
2022-08-19 20:43:46 +0000

    0x11 project a

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
-        sys.argv[2],
-        sys.argv[1])
-    )
+    res = requests.get(f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits')
     if res.status_code >= 400:
         print('None')
     else:
         for comment in res.json()[:10]:
-            print("{}: {}".format(
-                comment.get('sha'),
-                comment.get('commit').get('author').get('name'))
-            )
+            print(f"{comment.get('sha')}: {comment.get('commit').get('author').get('name')}")
