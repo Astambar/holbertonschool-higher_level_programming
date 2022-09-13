b1b8ef96398e2ad09d57dcd142320586b0e7edaf
Astambar <leuchartjason@gmail.com>
2022-09-13 12:58:49 +0000

    0x14

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 
-// Requiring fs module in which
-// readFile function is defined.
+
 const fs = require('fs')
   
-// Reading data in utf-8 format
-// which is a type of character set.
-// Instead of 'utf-8' it can be 
-// other character set also like 'ascii'
+
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
-    if (err) throw err;
-  
-    // Converting Raw Buffer to text
-    // data using tostring function.
-    return console.log(err);
+    if (err) { return console.log(err); }
 })
